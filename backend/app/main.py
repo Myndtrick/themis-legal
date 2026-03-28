1b71772e007d49ac042b5cd26194e5c3f9cc7218
Radu Gogoasa <radugogoasa@Radus-MacBook-Air.local>
2026-03-28 11:24:56 +0200

    fix: add port 4000 to CORS allowed origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -105,7 +105,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "http://localhost:4000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
